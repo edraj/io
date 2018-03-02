a8c17b0489cd63c71797f11f9c6e07dd11e29731
Kefah Issa <kefah.issa@gmail.com>
2018-03-02 04:05:59 -0800

    minor

--- tools/setup.sh
+++ tools/setup.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-sudo dnf install git-lfs protobuf-compiler mongodb mongodb-server golang
-sudo systemctl start mongodb
+sudo dnf install git-lfs protobuf-compiler mongodb mongodb-server golang mongo-tools
+sudo systemctl start mongod
+git lfs pull
 
 pip3 install --user grpcio-tools
-export GOPATH="$HOME/go/bin:$PATH"
-go get github.com/BurntSushi/toml gopkg.in/mgo.v2 github.com/gorilla/mux
+export GOPATH="$HOME/go"
 go get google.golang.org/grpc
-go get -u github.com/golang/protobuf/{proto,protoc-gen-go}
-go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
+go get -u github.com/golang/protobuf/proto
 go get -u github.com/golang/protobuf/protoc-gen-go
+go get gopkg.in/mgo.v2 
+
 mongo edraj --eval 'db.content.drop();'
 # mongodump -d edraj --gzip
-mongorestore -d edraj --gzip --dir ../sampledata/
+mongorestore -d edraj --gzip --dir ../sampledata/edraj
 mongo edraj --eval 'db.content.count();'
 mongo edraj --eval 'db.content.getIndices();'
 #mongo edraj --eval 'db.content.createIndex( { displayname: "text", body: "text" }, {"default_language":"none"} );' #  Arabic sadly requires 3rd party license
